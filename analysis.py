82dbcff2f7b0770b2997173395d40db3b133615c
Shyzay <chukwuemekadanchuku@gmail.com>
2018-09-01 18:43:43 +0100

    added analysis in algorithms and Data Structure

---
+++ analysis.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  1 15:54:53 2018
+
+@author: KELS
+"""
+
+# O(nlogn)
+def analysis():
+   random_list = [3,0,7, 4]    #list input
+   random_list.sort()         #Sort the list
+   print(random_list[0])      #the minimum will be at the first index after the sort
+
+analysis()
